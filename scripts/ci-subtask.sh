ad3387c37018683d8082539b7337fe0170e75cb9
Hu# <jinhao.hu@pingcap.com>
2024-04-09 16:45:21 +0800

    scripts: fix client and tools cover profile (#8020)
    ref tikv/pd#4399
    
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- scripts/ci-subtask.sh
+++ scripts/ci-subtask.sh
@@ -2,12 +2,12 @@
 
 # ./ci-subtask.sh <TOTAL_TASK_N> <TASK_INDEX>
 
-ROOT_PATH=../../
+ROOT_PATH_COV=$(pwd)/covprofile
 
 if [[ $2 -gt 9 ]]; then
     # run tools tests
     if [[ $2 -eq 10 ]]; then
-        cd ./tools && make ci-test-job && cd .. && cat ./covprofile >> covprofile || exit 1
+        cd ./tools && make ci-test-job && cat covprofile >> $ROOT_PATH_COV || exit 1
         exit
     fi
 
@@ -16,12 +16,12 @@ if [[ $2 -gt 9 ]]; then
     integrations_tasks=($(find "$integrations_dir" -mindepth 1 -maxdepth 1 -type d))
     for t in "${integrations_tasks[@]}"; do
         if [[ "$t" = "$integrations_dir/client" && $2 -eq 11 ]]; then
-            cd ./client && make ci-test-job && cd .. && cat ./covprofile >> covprofile || exit 1
-            cd $integrations_dir && make ci-test-job test_name=client && cat ./client/covprofile >> "$ROOT_PATH/covprofile" || exit 1
+            cd ./client && make ci-test-job && cat covprofile >> $ROOT_PATH_COV && cd .. || exit 1
+            cd $integrations_dir && make ci-test-job test_name=client && cat ./client/covprofile >> $ROOT_PATH_COV || exit 1
         elif [[ "$t" = "$integrations_dir/tso" && $2 -eq 12 ]]; then
-            cd $integrations_dir && make ci-test-job test_name=tso && cat ./tso/covprofile >> "$ROOT_PATH/covprofile" || exit 1
+            cd $integrations_dir && make ci-test-job test_name=tso && cat ./tso/covprofile >> $ROOT_PATH_COV || exit 1
         elif [[ "$t" = "$integrations_dir/mcs" && $2 -eq 13 ]]; then
-            cd $integrations_dir && make ci-test-job test_name=mcs && cat ./mcs/covprofile >> "$ROOT_PATH/covprofile" || exit 1
+            cd $integrations_dir && make ci-test-job test_name=mcs && cat ./mcs/covprofile >> $ROOT_PATH_COV || exit 1
         fi
     done
 else
@@ -61,5 +61,5 @@ else
         [[ $(($min_i + 1)) -eq $2 ]] && res+=($t)
     done
 
-    CGO_ENABLED=1 go test -timeout=15m -tags deadlock -race -covermode=atomic -coverprofile=covprofile -coverpkg=./... ${res[@]}
+    CGO_ENABLED=1 go test -timeout=15m -tags deadlock -race -covermode=atomic -coverprofile=$ROOT_PATH_COV -coverpkg=./... ${res[@]}
 fi
