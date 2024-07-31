45dac53d65c3903618e6a2a741fbd0bec20e3b44
Jack Lyu <63168620+JackL9u@users.noreply.github.com>
2024-07-31 16:22:21 +0800

    server: fix inappropriate log level (#8462)
    ref tikv/pd#8453
    
    changed log.Error to log.Warn, since it's a warning
    
    Signed-off-by: Boyang Lyu <boyangly@gmail.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- pkg/mcs/resourcemanager/server/manager.go
+++ pkg/mcs/resourcemanager/server/manager.go
@@ -121,7 +121,7 @@ func (m *Manager) Init(ctx context.Context) error {
 		return err
 	}
 	if err = json.Unmarshal([]byte(v), &m.controllerConfig); err != nil {
-		log.Error("un-marshall controller config failed, fallback to default", zap.Error(err), zap.String("v", v))
+		log.Warn("un-marshall controller config failed, fallback to default", zap.Error(err), zap.String("v", v))
 	}
 
 	// re-save the config to make sure the config has been persisted.
