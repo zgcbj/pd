32829f140db8a7b783668b8350e29d42c05c7aa4
Hu# <ihusharp@gmail.com>
2024-08-22 11:49:42 +0800

    tests: make TestAllocIDAfterLeaderChange more stable (#8558)
    close tikv/pd#8557
    
    need to wait leader
    
    Signed-off-by: husharp <ihusharp@gmail.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- tests/integrations/mcs/scheduling/server_test.go
+++ tests/integrations/mcs/scheduling/server_test.go
@@ -106,6 +106,7 @@ func (suite *serverTestSuite) TestAllocIDAfterLeaderChange() {
 	pd2, err := suite.cluster.Join(suite.ctx)
 	re.NoError(err)
 	err = pd2.Run()
+	re.NotEmpty(suite.cluster.WaitLeader())
 	re.NoError(err)
 	tc, err := tests.NewTestSchedulingCluster(suite.ctx, 1, suite.backendEndpoints)
 	re.NoError(err)
