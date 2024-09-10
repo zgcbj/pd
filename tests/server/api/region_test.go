f6ec3f3f138ce759e77201d8e233a983fe34634d
Ryan Leung <rleungx@gmail.com>
2024-09-10 17:02:03 +0800

    *: make `TestAccelerateRegionsScheduleInRanges` stable (#8604)
    close tikv/pd#8430
    
    Signed-off-by: Ryan Leung <rleungx@gmail.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- tests/server/api/region_test.go
+++ tests/server/api/region_test.go
@@ -138,7 +138,10 @@ func (suite *regionTestSuite) checkSplitRegions(cluster *tests.TestCluster) {
 }
 
 func (suite *regionTestSuite) TestAccelerateRegionsScheduleInRange() {
+	re := suite.Require()
+	re.NoError(failpoint.Enable("github.com/tikv/pd/pkg/schedule/checker/skipCheckSuspectRanges", "return(true)"))
 	suite.env.RunTestBasedOnMode(suite.checkAccelerateRegionsScheduleInRange)
+	re.NoError(failpoint.Disable("github.com/tikv/pd/pkg/schedule/checker/skipCheckSuspectRanges"))
 }
 
 func (suite *regionTestSuite) checkAccelerateRegionsScheduleInRange(cluster *tests.TestCluster) {
@@ -173,7 +176,10 @@ func (suite *regionTestSuite) checkAccelerateRegionsScheduleInRange(cluster *tes
 }
 
 func (suite *regionTestSuite) TestAccelerateRegionsScheduleInRanges() {
+	re := suite.Require()
+	re.NoError(failpoint.Enable("github.com/tikv/pd/pkg/schedule/checker/skipCheckSuspectRanges", "return(true)"))
 	suite.env.RunTestBasedOnMode(suite.checkAccelerateRegionsScheduleInRanges)
+	re.NoError(failpoint.Disable("github.com/tikv/pd/pkg/schedule/checker/skipCheckSuspectRanges"))
 }
 
 func (suite *regionTestSuite) checkAccelerateRegionsScheduleInRanges(cluster *tests.TestCluster) {
