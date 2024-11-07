b19cec5e944d1a5be09e85580c527a4b0b461adc
lhy1024 <liuhanyang@pingcap.com>
2024-11-07 14:57:39 +0800

    cluster: fix tso fallback when pd leader switch (#8783)
    close tikv/pd#8781
    
    Signed-off-by: lhy1024 <admin@liudos.us>

--- server/cluster/cluster.go
+++ server/cluster/cluster.go
@@ -460,7 +460,14 @@ func (c *RaftCluster) runServiceCheckJob() {
 		case <-schedulingTicker.C:
 			c.checkSchedulingService()
 		case <-tsoTicker.C:
-			c.checkTSOService()
+			// ensure raft cluster is running
+			// avoid unexpected startTSOJobsIfNeeded when raft cluster is stopping
+			// ref: https://github.com/tikv/pd/issues/8781
+			c.RLock()
+			if c.running {
+				c.checkTSOService()
+			}
+			c.RUnlock()
 		}
 	}
 }
@@ -488,6 +495,7 @@ func (c *RaftCluster) stopTSOJobsIfNeeded() error {
 		return err
 	}
 	if allocator.IsInitialize() {
+		log.Info("closing the global TSO allocator")
 		c.tsoAllocator.ResetAllocatorGroup(tso.GlobalDCLocation, true)
 		failpoint.Inject("updateAfterResetTSO", func() {
 			allocator, _ := c.tsoAllocator.GetAllocator(tso.GlobalDCLocation)
